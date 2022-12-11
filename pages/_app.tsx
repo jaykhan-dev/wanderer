7f0b0f297cdbdd10400e76f894671c0c9a8aa8c0
jaykhan-dev <jaykhan.sound@gmail.com>
2022-12-11 12:18:50 -0500

    wanderer first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,27 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { useState } from "react";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider, Session } from "@supabase/auth-helpers-react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({
+  Component,
+  pageProps,
+}: AppProps<{ initialSession: Session }>) {
+  const [supabase] = useState(() => createBrowserSupabaseClient());
+
+  return (
+    <SessionContextProvider
+      supabaseClient={supabase}
+      initialSession={pageProps.initialSession}
+    >
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </SessionContextProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
